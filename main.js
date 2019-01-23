e76eb8e16d3a9f85660e5028f9f3b2796fbf2ecc
MarceArratia <marratiaf@hotmail.com>
2019-01-22 22:29:26 -0300

    Actualizando html,main boton minecraft

--- main.js
+++ main.js
@@ -13,5 +13,9 @@ document.getElementById("pictureResident").addEventListener("click",function(){
     document.getElementById("video").innerHTML ="<iframe src='https://www.youtube.com/embed/wJbhS_mHo7Q' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen ></iframe>";
                                                 
 });
+document.getElementById("pictureMinecraft").addEventListener("click",function(){
+    searchMovie("Minecraft");
+    document.getElementById("video").innerHTML ="<iframe src='https://www.youtube.com/embed/z2nb_zAElJI' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen ></iframe>";                                            
+});
 
   
